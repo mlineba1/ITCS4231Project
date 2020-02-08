718637a8e73a3347e0aa184ac07f48a12cfda6c7
mlineba1 <mlineba1@uncc.edu>
2020-02-08 13:26:23 -0500

    Added files and a script
    File locations for organization, and a persistant data script

---
+++ ITCS 4231 Project/Assets/Scripts/PersistentData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentData : MonoBehaviour
+{
+    public static PersistentData instance;
+
+        private void Awake()
+        {
+                //If it doesnt exist, this is the first
+                if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+                //If it does exist, delete the duplicate
+                else
+            {
+            Destroy(gameObject);
+            }
+
+        }
+   
+}
