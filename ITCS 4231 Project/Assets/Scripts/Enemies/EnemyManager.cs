d0e17fa9514eeebd2dd6735dd0a9474a6e4a1d32
Milo Boger <aboger9@uncc.edu>
2020-02-23 15:49:50 -0500

    animation scripting put in enemy controller script, updated tags for players and monsters

--- ITCS 4231 Project/Assets/Scripts/Enemies/EnemyManager.cs
+++ ITCS 4231 Project/Assets/Scripts/Enemies/EnemyManager.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-	[SerializeField] private Animator animator;
+	[SerializeField] private Animator enemyAnim;
 	[SerializeField] private Transform trans;
 	[SerializeField] private float attackRange;
 	[SerializeField] private float aggroRadius;
-    [SerializeField] private Transform characterTrans;
+    [SerializeField] private Transform playerTrans;
 	private int maxHealth;
 	private int currentHealth;
 	private bool isAggro;
+	private bool isMoving;
+	private bool isAttacking;
 	private Vector3 spawnPoint;
 	private int attackType;
 	private bool isDead;
@@ -24,6 +26,7 @@ public class EnemyManager : MonoBehaviour
         spawnPoint = transform.position;
 		isDead = false;
 		isAggro = false;
+		isMoving = false;
     }
 
     // Update is called once per frame
@@ -37,7 +40,7 @@ public class EnemyManager : MonoBehaviour
 			yield return new WaitForSeconds(1f);
 
 			// Check distance to aggro (!!!)
-			float d = Vector3.Distance (trans.position, characterTrans.position); 
+			float d = Vector3.Distance (trans.position, playerTrans.position); 
 
 			if (d < aggroRadius) {
 				isAggro = true;
@@ -48,22 +51,43 @@ public class EnemyManager : MonoBehaviour
 
 	//following player
 	private void followPlayer(){
-		Vector3 towardsPlayer =characterTrans.position - trans.position;
+		
+		Vector3 towardsPlayer =playerTrans.position - trans.position;
 		Vector3 towardsSpawnPoint = trans.position - spawnPoint;
 
 		// Are we close enough to attack -> Attack state
 		if (isAggro==true && towardsPlayer.magnitude < attackRange) {
+			//set animator to not move
+			isMoving = false;
+			enemyAnim.SetBool("enemyMoving", false);
+
 			//insert some attacking code here (!!!)
-			//set the animator to attacking (!!!)
+
+			//set the animator to attacking 
+			isAttacking = true;
+			enemyAnim.SetBool("enemyAttacking", true);
+
+			//set up script for different attack types (!!!)
+			//set animator for the big attack
+			//enemyAnim.SetBool("enemyBigAttacking", true);
 		}
 		else{
 			//move closer to the player
-			//set animator moving (!!!)
+			isMoving = true;
+			//set animator for moving
+			enemyAnim.SetBool("enemyMoving", true);
 
 			towardsPlayer.Normalize ();
 			//towardsPlayer *= moveSpeed * Time.deltaTime;
 
 			trans.position += towardsPlayer;
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (collision.gameObject.tag == "Player"){
+			//set animator trigger for getting hit
+			animator.SetTrigger ("enemyHit");
 		}
 	}
 }
