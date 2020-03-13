dc0e4e982d279d9e9a216d9003d21f506ab0c00b
Milo Boger <aboger9@uncc.edu>
2020-03-13 19:24:37 -0400

    Enemy healthbars face players

---
+++ ITCS 4231 Project/Assets/Scripts/UI Scripts/FaceCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    public Camera playerCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = playerCamera.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(playerCamera.transform.position - v);
+        transform.Rotate(0, 180, 0);
+    }
+}
